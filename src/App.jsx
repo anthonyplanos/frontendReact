5250ead6fc788e565837f6d4fda3f83c9266aebb
Anthony Planos <planosant14@gmail.com>
2026-04-08 15:50:40 +0800

    Added admin auth

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,26 @@ import Login from "./components/admin/Login"
 import { ToastContainer, toast } from 'react-toastify';
 import Dashboard from "./components/admin/Dashboard";
 import { AdminRequireAuth } from "./components/admin/AdminRequireAuth";
+import { AdminAuthProvider } from "./components/context/AdminAuth";
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/admin/login" element={<Login />} />
-          <Route path="/admin/dashboard" element={
-            <AdminRequireAuth>
-              <Dashboard />
-            </AdminRequireAuth>
-          } />
-        </Routes>
-      </BrowserRouter>
+      <AdminAuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/admin/login" element={<Login />} />
+            <Route path="/admin/dashboard" element={
+              <AdminRequireAuth>
+                <Dashboard />
+              </AdminRequireAuth>
+            } />
+          </Routes>
+        </BrowserRouter>
+      </AdminAuthProvider>
 
       <ToastContainer />
     </>
--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Layout from '../common/Layout'
+import { AdminAuthContext } from '../context/AdminAuth';
 
 const Dashboard = () => {
+  const {logout} = useContext(AdminAuthContext);
   return (
     <Layout>
       <div>Dashboard</div>
+      <button className='btn btn-primary' onClick={logout}>Logout</button>
     </Layout>
   )
 }
